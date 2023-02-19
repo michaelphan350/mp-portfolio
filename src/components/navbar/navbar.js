d7285e0bb1fb9bf92a5ffd49e7640870b5f6f8df
Michael <michaelphan350@gmail.com>
2023-02-18 20:15:06 -0700

    more navbar stylings 1

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -3,8 +3,7 @@ import "./navbar.css"
 export default function Navbar() {
     return (
         <div className='navbar' id='navbar'>
-            <h1>Michael Phan</h1>
-            <h1>Full Stack Developer</h1>
+            <h1>Michael Phan: Full Stack Developer</h1>
         </div>
     );
 }
